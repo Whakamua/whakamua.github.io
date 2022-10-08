50ae5e29e7e424a8228992df6aed3481428ea293
Jordy Dikkers <jordydikkers@phaidra.ai>
2022-10-08 11:19:50 +0800

    add libs

---
+++ libs/utils.js
@@ -0,0 +1,14 @@
+function argmax(arr) {
+  // return the index of the element with the highest value.
+  var max = arr[0];
+  var maxIndex = 0;
+
+  for (var i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      maxIndex = i;
+    }
+  }
+
+  return maxIndex;
+}
